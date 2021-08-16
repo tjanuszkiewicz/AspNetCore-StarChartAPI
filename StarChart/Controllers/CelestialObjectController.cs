73236434fc659d2d227fc6caa17268031b6021d6
tjanuszkiewicz <t.januszkiewicz@tlen.pl>
2021-08-16 14:18:06 +0200

    WQQQW

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectController: ControllerBase
+    {
+        [Route("")]
+        [ApiController] 
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
