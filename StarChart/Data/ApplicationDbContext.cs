a86feaf262de7d65bfdd11bac7a204fb5c5e1771
tjanuszkiewicz <t.januszkiewicz@tlen.pl>
2021-08-16 14:08:04 +0200

    WQRQWE

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,9 +5,10 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects;
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            DbSet<CelestialObject> CelestialObjects;
         }
     }
 }
